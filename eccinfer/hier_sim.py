680b8da1e1e3b30875795e31c11170f4693e943d
vighnesh-nagpal <48160445+vighnesh-nagpal@users.noreply.github.com>
2022-07-20 11:28:04 -0700

    Update hier_sim.py

--- eccinfer/hier_sim.py
+++ eccinfer/hier_sim.py
@@ -71,11 +71,6 @@ class Pop_Likelihood(object):
             self.posteriors=None
             self.ecc_posteriors=posteriors
 
-            for post in self.ecc_posteriors:
-                print(post)
-                # post[np.where(post==0)]=0.001
-                # post[np.where(post==1)]=0.999
-
             self.beta_max=beta_max
             self.prior_type=prior
 
@@ -124,10 +119,6 @@ class Pop_Likelihood(object):
         system_sums = np.array([np.sum(beta.pdf(ecc_post,a,b))/(np.shape(ecc_post)[0])
                         for ecc_post in self.ecc_posteriors])
 
-        # print(beta.pdf(self.ecc_posteriors[3],a,b))
-        # print(self.ecc_posteriors[3][np.where(beta.pdf(self.ecc_posteriors[3],a,b)==np.inf)])
-
-        # print(system_sums)
 
         log_likelihood = np.sum(np.log(system_sums))
 
@@ -176,178 +167,7 @@ class Pop_Likelihood(object):
 
         return beta_post
 
-
-
-def experiment():
-
-    ns=[5,10,20,50]
-    sigs=[0.2,0.05,0.01]
-
-    a = 0.867
-    b = 3.03
-
-    for N in ns:
-    
-        eccentricities   = np.random.beta(a,b,size=N)
-
-        for sig in sigs:
-
-            ecc_posts=[np.random.normal(ecc, sig,5000) for ecc in eccentricities]
-
-            like=Pop_Likelihood(posteriors=ecc_posts,prior='Gaussian',mu=0.6867,std=1.0)
-            beta_samples=like.sample(2000,burn_steps=200,nwalkers=20)
-            np.save(f'./gaussian_experiment/wj/shifted_gaussian_bigstd/beta_posts/{str(N)}_{str(int(100*sig))}',beta_samples)
-
-            fig=corner.corner(beta_samples,labels=['a','b'])
-            plt.savefig(f'./gaussian_experiment/wj/shifted_gaussian_bigstd/beta_corners/{str(N)}_{str(int(100*sig))}.png')
-
-            # like=Pop_Likelihood(posteriors=ecc_posts)
-            # beta_samples=like.sample(2000,burn_steps=200,nwalkers=20)
-            # np.save(f'./gaussian_experiment/a4b2/uniform/beta_posts/{str(N)}_{str(int(100*sig))}',beta_samples)
-
-            # fig=corner.corner(beta_samples,labels=['a','b'])
-            # plt.savefig(f'./gaussian_experiment/a4b2/uniform/beta_corners/{str(N)}_{str(int(100*sig))}.png')
-
-            # like=Pop_Likelihood(posteriors=ecc_posts,prior='log-uniform')
-            # beta_samples=like.sample(2000,burn_steps=200,nwalkers=20)
-            # np.save(f'./gaussian_experiment/a4b2/log_uniform/beta_posts/{str(N)}_{str(int(100*sig))}',beta_samples)
-
-            # fig=corner.corner(beta_samples,labels=['a','b'])
-            # plt.savefig(f'./gaussian_experiment/a4b2/log_uniform/beta_corners/{str(N)}_{str(int(100*sig))}.png')
-
-def four_priors(fnames,dir,nsteps=15000,burn_steps=2000,test=False):
-
-    if test:
-        nsteps=50
-        burn_steps=20
-
-        
-    # # log-uniform prior
-    # like=Pop_Likelihood(fnames=fnames,prior='log-uniform',beta_max=100)
-    # beta_samples=like.sample(nsteps,burn_steps=burn_steps,nwalkers=20)
-    # np.save(f'{dir}/loguniform_100',beta_samples)
-    # fig=corner.corner(beta_samples,labels=['a','b'])
-    # plt.savefig(f'{dir}/loguniform_100_corner.png')
-
-    # # Log Normal prior
-
-    # like=Pop_Likelihood(fnames=fnames,prior='LogNormal',beta_max=100)
-    # beta_samples=like.sample(nsteps,burn_steps=burn_steps,nwalkers=20)
-    # np.save(f'{dir}/lognormal',beta_samples)
-    # fig=corner.corner(beta_samples,labels=['a','b'])
-    # plt.savefig(f'{dir}/lognormal_corner.png')
-
-    # # Uniform Prior
-
-    # like=Pop_Likelihood(fnames=fnames,prior=None,beta_max=100)
-    # beta_samples=like.sample(nsteps,burn_steps=burn_steps,nwalkers=20)
-    # np.save(f'{dir}/uniform',beta_samples)
-    # fig=corner.corner(beta_samples,labels=['a','b'])
-    # plt.savefig(f'{dir}/uniform_corner.png')
-
-    # Gaussian Prior
-
-    like=Pop_Likelihood(fnames=fnames,prior='Gaussian',beta_max=100,mu=0.6867,std=1.0)
-    beta_samples=like.sample(nsteps,burn_steps=burn_steps,nwalkers=20)
-    np.save(f'{dir}/shifted_gaussian_bigstd',beta_samples)
-    fig=corner.corner(beta_samples,labels=['a','b'])
-    plt.savefig(f'{dir}/shifted_gaussian_bigstd_corner.png')
-
-
-
-def sim_forward_modelling():
-
-    ### ALL SYSTEMS: uniform DISTRIBUTION
-    
-    uniform_all_fnames=sorted(glob.glob('/data/user/vnagpal/eccentricities/e2esims/days/uniform_e2e_sims/fixed_inc/*'))
-    
-    # 5 systems:
-    n_systems=5
-    sample_systems=np.random.randint(0,high=len(uniform_all_fnames)-1,size=n_systems)
-    sys_fnames=[uniform_all_fnames[sys] for sys in sample_systems]
-
-    four_priors(sys_fnames,dir='/home/vnagpal/eccentricities/e2esims/days/uniform_e2e_sims/5_5/fixed_inc/5systems')
-
-    # 10 systems
-    n_systems=10
-    sample_systems=np.random.randint(0,high=len(uniform_all_fnames)-1,size=n_systems)
-    sys_fnames=[uniform_all_fnames[sys] for sys in sample_systems]
-
-    four_priors(sys_fnames,dir='/home/vnagpal/eccentricities/e2esims/days/uniform_e2e_sims/5_5/fixed_inc/10systems')
-
-
-    # 20 systems
-    n_systems=20
-    sample_systems=np.random.randint(0,high=len(uniform_all_fnames)-1,size=n_systems)
-    sys_fnames=[uniform_all_fnames[sys] for sys in sample_systems]
-
-    four_priors(sys_fnames,dir='/home/vnagpal/eccentricities/e2esims/days/uniform_e2e_sims/5_5/fixed_inc/20systems')
-    
-    # all systems
-    n_systems=45
-    sample_systems=np.random.randint(0,high=len(uniform_all_fnames)-1,size=n_systems)
-    sys_fnames=[uniform_all_fnames[sys] for sys in sample_systems]
-    
-    four_priors(sys_fnames,dir='/home/vnagpal/eccentricities/e2esims/days/uniform_e2e_sims/5_5/fixed_inc/allsystems')
-
-
-def sim_observational_sample(nsteps,burn_steps):
-
-    # construct samples
-
-    fnames=sorted(glob.glob('/home/vnagpal/eccentricities/ogpaper3/individual_posts/*.npy'))
-    # print(fnames)
-    planets=['51erib','hr8799b','hr8799c','hr8799d','hr8799e','pds70b','hd95086b','picb','hip65426b']
-    planet_fnames={}
-    bd_fnames=[]
-    for f in fnames:
-        is_planet=False
-        for p in planets:
-            if p in f:
-                planet_fnames[p]=f
-                is_planet=True
-        if not is_planet:
-            bd_fnames.append(f)
-
-    for planet in planet_fnames:
-        planet_post=np.load(planet_fnames[planet],allow_pickle=True)
-        fig=plt.figure()
-        plt.hist(planet_post,bins=50)
-        plt.savefig(f'/home/vnagpal/eccentricities/ogpaper3/individual_posts/{planet}_post')
-
-    bd_posts=[np.load(bd) for bd in bd_fnames]
-    print(planet_fnames)
-    planet_posts=[np.load(planet_fnames[planet]) for planet in planet_fnames]
-
-
-    like=Pop_Likelihood(posteriors=planet_posts,prior='Gaussian',beta_max=100,mu=0.6867,std=1.0)
-    beta_samples=like.sample(nsteps,burn_steps=burn_steps,nwalkers=20)
-    np.save('/home/vnagpal/eccentricities/ogpaper3/gp_shifted_bigstd_gaussian',beta_samples)
-    fig=corner.corner(beta_samples,labels=['a','b'])
-    plt.savefig('/home/vnagpal/eccentricities/ogpaper3/gp_shifted_bigstd_gaussian_corner.png')
-
-    # like=Pop_Likelihood(posteriors=bd_posts,prior='log-uniform',beta_max=100)
-    # beta_samples=like.sample(nsteps,burn_steps=burn_steps,nwalkers=20)
-    # np.save('/home/vnagpal/eccentricities/ogpaper3/bd_log_uniform',beta_samples)
-    # fig=corner.corner(beta_samples,labels=['a','b'])
-    # plt.savefig('/home/vnagpal/eccentricities/ogpaper3/bd_log_uniform_corner.png')
-
-    like=Pop_Likelihood(posteriors=planet_posts,prior=None,beta_max=10)
-    beta_samples=like.sample(nsteps,burn_steps=burn_steps,nwalkers=20)
-    np.save('/home/vnagpal/eccentricities/ogpaper3/gp_uniform_10',beta_samples)
-    fig=corner.corner(beta_samples,labels=['a','b'])
-    plt.savefig('/home/vnagpal/eccentricities/ogpaper3/gp_uniform_10_corner.png')
-
-if __name__ == '__main__':
-    # experiment()
-    # sim_forward_modelling()
-
-    sim_observational_sample(nsteps=15000,burn_steps=2000)
-
-
-
-
-    
+  
    
 
 
