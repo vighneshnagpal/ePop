010415680cdc3e6253ea08093c02f29d033c5da0
vighnesh-nagpal <48160445+vighnesh-nagpal@users.noreply.github.com>
2022-09-07 00:11:26 -0700

    Update plot.py

--- ePop/plot.py
+++ ePop/plot.py
@@ -244,13 +244,4 @@ def plot_single(fname,true_beta,savename,title=None,true_label='underlying',nran
     plt.close()
     return fig
 
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
+
